53eedfe12ddeb8479a906e5b2746b0d5270e22e4
sabakr <sabakrfutu@gmail.com>
2026-08-01 20:14:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,7 @@ with c4:
         st.text_input("البريد الإلكتروني", placeholder="أدخل البريد الإلكتروني")
         
 st.date_input("تاريخ التوظيف *")
-    
-    st.write("")
+st.write("")
     b1, b2, b3, b4, b5, b6 = st.columns(6)
     with b6: st.button("➕ إضافة", use_container_width=True)
     with b5: st.button("📝 تعديل", use_container_width=True)
